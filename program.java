30906ef3be28c730f1bff2cfe15d4634e7380717
sakthidevi17 <23203023@srcas.ac.in>
2024-07-01 12:55:18 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,9 @@
+class program {
+  public static void main(String[]args)
+  {
+    System.out.println("hello");
+    System.out.println("to");
+    System.out.println("devops");
+    System.out.println("lab");
+  }
+}
